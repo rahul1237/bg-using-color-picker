80e29ee09cffe10cafd89e8d0051a39674293ecc
Rahul Mahajan <rahulmahajan98645@gmail.com>
2021-02-09 00:40:10 +0530

    bg color using clr picker

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const bgclr = document.getElementById('clr')
+const hellow = document.querySelector('.hello')
 
 bgclr.addEventListener('input',()=>{
     document.body.style.backgroundColor = bgclr.value
+
+    console.log(bgclr.value);
+    if(bgclr.value == "#000000"){
+    hellow.style.color = '#fff'
+    }
 })
